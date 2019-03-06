d3853630e945f9265b5b14a459c75f469187bc9c
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-05 21:45:52 -0600

    Added get posts

--- postRoutes.js
+++ postRoutes.js
@@ -4,4 +4,14 @@ const router = express.Router();
 
 router.use(express.json());
 
+router.get('/posts', (req, res) => {
+  db.get()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({error: 'The posts could not be retrieved!'});
+    });
+});
+
 module.exports = router;
