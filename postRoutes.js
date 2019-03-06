4d9120e6e1914fe321051e74ecaf0624e9a78edd
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-05 22:05:50 -0600

    Added get post by id

--- postRoutes.js
+++ postRoutes.js
@@ -14,4 +14,19 @@ router.get('/posts', (req, res) => {
     });
 });
 
+router.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.getById(id)
+    .then(posts =>{
+      if(posts){
+        res.json(posts)
+      }else{
+        res.status(404).json({message: 'The post with specified ID does not exist!'})
+      }
+    }).catch(err => {
+      res.status(500).json({error: 'The post could not be retrieved.'})
+    });
+});
+
 module.exports = router;
