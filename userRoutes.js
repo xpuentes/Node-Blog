69f7dda0926452e80423f8d4b2ed4329c25fd44b
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-05 21:13:40 -0600

    Added get posts by user id

--- userRoutes.js
+++ userRoutes.js
@@ -25,7 +25,22 @@ router.get('/users/:id', (req, res) => {
       }
     })
     .catch(err => {
-      res.status(500).json({errorMessage: 'User data could not be retrieved!'})
+      res.status(500).json({errorMessage: 'User data could not be retrieved!'});
+    });
+});
+
+router.get('/users/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.getUserPosts(id)
+    .then(users => {
+      if(users[0]){
+        res.json(users);
+      } else {
+        res.status(404).json({message: 'User with specified ID not found!'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage: 'User data could not be retrieved!'});
     });
 });
 
